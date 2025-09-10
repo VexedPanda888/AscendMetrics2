6f4bc0cf2965fa6c9566b3979cd27dd23dc7a3d0
Connor Hay <cwhay888@gmail.com>
2025-09-10 18:01:01 -0400

    Navigation bar front end layed in

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -4,12 +4,14 @@ import ActivityDataGrid from "./components/data-grid/ActivityDataGrid";
 import BalanceLineChart from "./components/charts/BalanceLineChart";
 import activityGridColumns from "./components/data-grid/ActivityColumns";
 import demoRows from "./components/data-grid/DemoActivityData";
+import SimpleBottomNavigation from "./components/NavigationBar";
 
 function App() {
   return (
     <Box sx={{ height: "100%", width: "100%" }}>
       <BalanceLineChart />
       <ActivityDataGrid columns={activityGridColumns} rows={demoRows} />
+      <SimpleBottomNavigation />
     </Box>
   );
 }
--- react-ts/src/components/NavigationBar.tsx
+++ react-ts/src/components/NavigationBar.tsx
@@ -1,2 +1,52 @@
-// places / sign in / sign up
-// can be horizontal top, horizontal bottom
+import * as React from "react";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SettingsIcon from "@mui/icons-material/Settings";
+import Paper from "@mui/material/Paper";
+
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
+  const isSignedIn = true;
+
+  return (
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        {isSignedIn ? (
+          <>
+            <BottomNavigationAction
+              label="Sign In"
+              showLabel
+              icon={<LoginIcon />}
+            />
+            <BottomNavigationAction
+              label="Sign Up"
+              showLabel
+              icon={<PersonAddAltIcon />}
+            />
+          </>
+        ) : (
+          <>
+            <BottomNavigationAction
+              label="Sign Out"
+              showLabel
+              icon={<LogoutIcon />}
+            />
+          </>
+        )}
+        <BottomNavigationAction label="Settings" icon={<SettingsIcon />} />
+      </BottomNavigation>
+    </Paper>
+  );
+}
