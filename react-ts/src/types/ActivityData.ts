59d5db94fd59deb7503eb1f51b14552f5d836b69
Connor Hay <cwhay888@gmail.com>
2025-10-18 20:19:47 -0400

    added description for activity data type

--- react-ts/src/types/ActivityData.ts
+++ react-ts/src/types/ActivityData.ts
@@ -1,3 +1,7 @@
+/**
+ * This activity data type represents a single activity entry with varied intensity and performance metrics.
+ * This type is used primarily for displaying activity data in data grids.
+ */
 export interface ActivityData {
   id: number;
   date: Date;
