f8773119bc44f3a32c31a8a9b0674fa8b62ae50d
Connor Hay <cwhay888@gmail.com>
2025-10-20 09:26:13 -0400

    fleshing out tyeps for user and activity data

--- react-ts/src/types/ActivityData.ts
+++ react-ts/src/types/ActivityData.ts
@@ -12,3 +12,16 @@ export interface ActivityData {
   lowerIntensity: number; // 0 to 5
   performance: number; // 0 to 5
 }
+
+export function isValidActivityData(data: ActivityData): data is ActivityData {
+  return (
+    typeof data.id === "number" &&
+    data.date instanceof Date &&
+    typeof data.activityName === "string" &&
+    typeof data.activityDuration === "number" &&
+    typeof data.upperIntensity === "number" &&
+    typeof data.fingersIntensity === "number" &&
+    typeof data.lowerIntensity === "number" &&
+    typeof data.performance === "number"
+  );
+}
