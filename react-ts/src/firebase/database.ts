eea09cb2f08bcec06d83c6d934d06e753b032d16
Connor Hay <cwhay888@gmail.com>
2025-10-20 16:47:19 -0400

    TODO's for endpoints

---
+++ react-ts/src/firebase/database.ts
@@ -0,0 +1,7 @@
+// TODO:
+// Set up Firebase Realtime Database functions here
+// GetOrCreateUserData - occurs on user sign in
+// CreateActivityData - occurs on new activity creation
+// GetActivityDataForUser - occurs on user sign in
+// UpdateActivityData - occurs on activity data edit
+// DeleteActivityData - occurs on activity data deletion
