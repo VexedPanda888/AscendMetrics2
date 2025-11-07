9f2b29daeac06be803d6661102c6b816bcfaef96
Connor Hay <cwhay888@gmail.com>
2025-11-07 09:46:47 -0500

    implimented EWMA calculation

---
+++ react-ts/src/metrics/calculateEWMA.ts
@@ -0,0 +1,20 @@
+import type { TimeSeries } from "../types/TimeSeries";
+
+/**
+ * calculates an exponentially weighted moving average.
+ */
+export default function calculateEWMA(
+  numberOfDaysInWindow: number,
+  timeSeries: TimeSeries
+): TimeSeries {
+  const alpha = 2 / (numberOfDaysInWindow + 1);
+  const ewmaDates = timeSeries.dates;
+  const ewmaValues = [];
+  ewmaValues[0] = timeSeries.values[0];
+  for (let i = 1; i <= timeSeries.dates.length; i++) {
+    ewmaValues[i] =
+      ewmaValues[i - 1] * (1 - alpha) + timeSeries.values[i] * alpha;
+  }
+  const ewma = { dates: ewmaDates, values: ewmaValues };
+  return ewma;
+}
